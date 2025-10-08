6af3c3cce36c68c75aa5ba789669e35b897f5327
WakamatuSanga <k024g0094@g.neec.ac.jp>
2025-10-08 15:05:21 +0900

    名前変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -6,7 +6,7 @@ using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// 初期化処理
-	KamataEngine::Initialize(L"LE2D_26_ワカマツ_サンガ_AL3");
+	KamataEngine::Initialize(L"3Dアクションゲーム");
 
 	//DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
